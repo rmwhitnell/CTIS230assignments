b842816725549fd79c3e47901f6dd8ffaf42cbc9
Rob Whitnell <rwhitnel@guilford.edu>
2020-05-01 11:06:21 -0400

    Additional work on image expansion
    Added ability to expand and shrink images with clicks

--- jstesting/testjs.js
+++ jstesting/testjs.js
@@ -5,6 +5,8 @@
 ourText = document.getElementById("text");
 ourButton = document.getElementById("mybutton");
 theTextInput = document.getElementById("myinput");
+theImgDiv = document.getElementById("card");
+theImage = document.getElementById("cardimage");
 
 // Each page is called "document"--we want to ask document to do something. That something is get the HTML node with the id "text"
 // Then, we'll give that element the name "ourText"
@@ -16,29 +18,46 @@ ourText.addEventListener("mouseout", onMouseOut);
 ourButton.addEventListener("click", onClick);
 theTextInput.addEventListener("blur", changeText); //blur is when the textfield loses focus; user clicks or tabs out of it
 theTextInput.addEventListener("change", changeText); //change is when user hits enter
+theImgDiv.addEventListener("click", expandImage);
 
 // We've now told ourText that it should use an event listener named onMouseOver whenever the mouseover event occurs--Table 21-2, pg. 614
 
 // Step 3: Write the event listener
+
+function expandImage() {
+    if (theImgDiv.style.position == "") {// if the div has its original value of position
+        theImgDiv.style.position = "absolute";
+        theImgDiv.style.top = "100";
+        theImgDiv.style.left = "100";
+        theImage.style.width = "auto";
+        theImage.src = "2_of_diamonds.png";
+    } // auto means what this width property would do if there was not a stylesheet
+    else { // make it small again
+        theImgDiv.style.position = "";
+        theImage.style.width = "100";
+    }
+}
 
 function changeText() {
     // get the text that the user entered
     newtext = theTextInput.value;
     // use that text to change text on the web page
     ourText.innerHTML = newtext;
+    theTextInput.value = "";
 }
 
 function onClick() {
-  ourText.innerHTML = "I clicked the button";
+    theImgDiv.style.display = "block";
+    ourText.innerHTML = "I clicked the button";
 }
 
 function onMouseOut() {
-  // If we want to return to the default property value, use "" 
-  ourText.style.color ="";
-  ourText.style.fontSize = "";
+    // If we want to return to the default property value, use "" 
+    ourText.style.color = "";
+    ourText.style.fontSize = "";
 }
 
 function onMouseOver() {
-  ourText.style.color = "green"; 
-  ourText.style.fontSize = "1.5em";
+    ourText.style.color = "green";
+    ourText.style.fontSize = "1.5em";
 }
