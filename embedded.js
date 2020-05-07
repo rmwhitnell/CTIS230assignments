3e5808cf25a0a5fd1db403746187eac86fc174aa
Rob Whitnell <rwhitnel@guilford.edu>
2020-05-07 10:36:19 -0400

    Add embedded apps testing
    Plotly
    SVG-edit

---
+++ embedded.js
@@ -0,0 +1,5 @@
+testplot = document.getElementById('testplotly'); // get the div that plotly controls
+Plotly.newPlot(testplot, [{
+    x: [0, 10, 20, 30, 40],
+    y: [8.497, 10.41, 11.033, 11.426, 11.802]
+}])
